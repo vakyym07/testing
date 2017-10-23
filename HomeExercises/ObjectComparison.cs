f11f734e32eccb4c3a27e91dffab2cd0af9ffd89
Артём Солдатенко < >
2017-10-23 22:32:37 +0500

    Fixed CheckCurrentTsar and added new comments

--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -4,35 +4,30 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
+    public class ObjectComparison
+    {
         [Test]
-	    [Description("Проверка текущего царя")]
-	    [Category("ToRefactor")]
-	    public void CheckCurrentTsar()
-	    {
-	        var actualTsar = TsarRegistry.GetCurrentTsar();
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
 
-	        var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-	            new Person("Vasili III of Russia", 28, 170, 60, null));
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
-	        // Перепишите код на использование Fluent Assertions.
-	        var chekingField = new List<string>() {"Name", "Age", "Height", "Weight"};
-	        foreach (var field in chekingField)
-	        {
-	            var fld = typeof(Person).GetField(field);
-	            fld.GetValue(actualTsar).Should().Be(fld.GetValue(expectedTsar));
-	            fld.GetValue(actualTsar.Parent).Should().Be(fld.GetValue(expectedTsar.Parent));
-	        }
-	    }
+            // Перепишите код на использование Fluent Assertions.
+            actualTsar.ShouldBeEquivalentTo(expectedTsar, options =>
+                options.Excluding(o => o.SelectedMemberPath.EndsWith("Id")));
+        }
 
-	    [Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-			new Person("Vasili III of Russia", 28, 170, 60, null));
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            var actualTsar = TsarRegistry.GetCurrentTsar();
+            var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
 
             // Какие недостатки у такого подхода? 
             // Недостатки этого подхода заключаются в том, что при добавлении новых свойств в 
@@ -41,49 +36,49 @@ namespace HomeExercises
             // Если сравниваемых полей достаточно много, метод раздуется, станет нечитаемым. 
             // 
             // Возможно, если родителей довольно много, из-за рекурсии можем получить StackOverflowException
+            // Если поменять тип целочисленных полей на тип с плавающей точкой, то такой метод сравнения не подойдёт.
             Assert.True(AreEqual(actualTsar, expectedTsar));
+        }
 
-		}
+        private bool AreEqual(Person actual, Person expected)
+        {
+            if (actual == expected) return true;
+            if (actual == null || expected == null) return false;
+            return
+                actual.Name == expected.Name
+                && actual.Age == expected.Age
+                && actual.Height == expected.Height
+                && actual.Weight == expected.Weight
+                && AreEqual(actual.Parent, expected.Parent);
+        }
+    }
 
-		private bool AreEqual(Person actual, Person expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-			actual.Name == expected.Name
-			&& actual.Age == expected.Age
-			&& actual.Height == expected.Height
-			&& actual.Weight == expected.Weight
-			&& AreEqual(actual.Parent, expected.Parent);
-		}
-	}
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person Parent;
+        public int Id;
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person Parent;
-		public int Id;
-
-		public Person(string name, int age, int height, int weight, Person parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+        public Person(string name, int age, int height, int weight, Person parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
